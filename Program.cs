2efb51cdbfb9c6461300c6baee92a20bd78c9095
Tigoh28 <ynv8yyzchb@privaterelay.appleid.com>
2026-06-09 22:08:01 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,33 +1,25 @@
-using GLMS.API_.Data;
-using GLMS.API_.Repositories;
-using GLMS.API_.Services;
-using Microsoft.EntityFrameworkCore;
+using GLMS.Client.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configure database setup using the verified configuration space
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddControllersWithViews();
 
-builder.Services.AddScoped<IContractRepository, ContractRepository>();
-builder.Services.AddScoped<LogisticsService>();
-builder.Services.AddHttpClient();
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Register the custom decoupled service wrapper over HttpClient
+builder.Services.AddHttpClient<ContractGatewayService>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+	app.UseExceptionHandler("/Home/Error");
 }
+app.UseStaticFiles();
 
+app.UseRouting();
 app.UseAuthorization();
-app.MapControllers();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Contract}/{action=Index}/{id?}");
 
 app.Run();
-
-public partial class Program { }
